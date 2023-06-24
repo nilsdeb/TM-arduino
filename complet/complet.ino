057f15917c4b322ddfb0ae56e63a9a282a7ace7b
blake <99916948+nilsdeb@users.noreply.github.com>
2023-06-24 18:17:13 +0200

    Update complet.ino

--- complet/complet.ino
+++ complet/complet.ino
@@ -1,4 +1,14 @@
 
+
+///////////////////////////////   structure   /////////////////////////////////////////
+
+
+
+
+
+
+
+
 ///////////////////////////////   librairie   /////////////////////////////////////////
 
 
@@ -24,8 +34,11 @@ const int chipSelect = 10;
 // defini pin bouton (a changer)
 const int boutonPin = 2;
 
-//defini pin GPS
+//defini pin GPS RX = reception    TX = transmission
 static const int RXPin = 3, TXPin = 4;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 // defini communication avec GPS
 static const uint32_t GPSBaud = 9600;
@@ -33,15 +46,32 @@ static const uint32_t GPSBaud = 9600;
 // Variable si ca mesure ou pas
 bool mesureEnCours = false;
 
+// The TinyGPS++ object
+TinyGPSPlus gps;
 
-
+// Objet de fichier pour la carte SD
+File dataFile;
 
 
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // moniteur
+  Serial.begin(9600);
+
+  // allume IMU
+  IMU.begin();
+
+  //gps
+  ss.begin(GPSBaud);
+
+  // Configure le bouton en entrée
+  pinMode(boutonPin, INPUT);
+
+  
+  
 
 }
 
