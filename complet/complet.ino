0512a996893befae00fe147fd9376e9f8ff2fba5
blake <99916948+nilsdeb@users.noreply.github.com>
2023-07-15 22:15:12 +0200

    Update complet.ino

--- complet/complet.ino
+++ complet/complet.ino
@@ -44,7 +44,7 @@ SoftwareSerial ss(RXPin, TXPin);
 static const uint32_t GPSBaud = 9600;
 
 // Variable si ca mesure ou pas
-bool mesureEnCours = false;
+bool mesure = false;
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -54,28 +54,128 @@ File dataFile;
 
 
 
-
-
 void setup() {
 
-  // moniteur
+  // met en place la communication avec le moniteur, que utilie avec l ordi
   Serial.begin(9600);
 
   // allume IMU
   IMU.begin();
 
-  //gps
+  // gps
   ss.begin(GPSBaud);
 
   // Configure le bouton en entrée
   pinMode(boutonPin, INPUT);
 
+  // allume la carte sd
+  if (!SD.begin(chipSelectPin)) {
+
+    // si elle s allume pas attendre 50 ms
+    delay(50);
+
+    // reessayer
+    SD.begin(chipSelectPin);
+  }
+  // ouverture du fichier
+  dataFile = SD.open("donnees.txt", FILE_WRITE);
+
+  // Vérification si le fichier est ouvert avec succès
+  if (!dataFile) {
+    Serial.println("erreur d'ouverture");
+    while (1);
+  }
+}
+
+
+void loop() {
+
+  // verifie l etat du bouton
+  if (digitalRead(boutonPin) == HIGH) {
+
+    // attend 50 ms
+    delay(50);
+
+    // verifie si le bouton est toujours allume
+    if (digitalRead(boutonPin) == HIGH) {
+
+      // verifie la constente mesure  ! inverse la logique, donc demare quand c est faux
+      if (!mesure) {
+
+        // change l etat de la variable
+        mesure = true;
+        
+        // Ouverture du fichier pour écriture en ajoutant les données
+        dataFile = SD.open("donnees.txt", FILE_WRITE);
+
+          // controle de l'ouverture
+          while (!dataFile) {
+            
+          Serial.println("Erreur lors de l'ouverture du fichier !");
+          
+          delay(1000);
+
+          // Réessayer l'ouverture du fichier
+          dataFile = SD.open("donnees.txt", FILE_WRITE); 
+          }
+        } 
+      } else {
+      
+      // change l etat de la variable
+      mesure = false;
+
+      // ferme le fichier
+      dataFile.close();    
+      }
+  }
+
+  // verifie si la constante
+  if (mesure) {
+
+    // precise ce qu est mesure
+    float mesure = lireMesure();
+
+    // imprime sur le serial
+    Serial.println(mesure);
+
+    // Écrire la mesure dans le fichier sur la carte SD
+    dataFile.println(mesure);  
+
+  } else {
+}
+     
+// code de lecture de l imu et du gps
+float lireMesure() {
+
+
+
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+
+
+
+        
   
-  
+
+
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+
+
+  
 
 }
