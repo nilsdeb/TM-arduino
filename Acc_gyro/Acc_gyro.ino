e7e7d1a2d14132cdd59d1cbc4806c071e9d12643
blake <99916948+nilsdeb@users.noreply.github.com>
2023-10-06 12:24:00 +0200

    Update Acc_gyro.ino

--- Acc_gyro/Acc_gyro.ino
+++ Acc_gyro/Acc_gyro.ino
@@ -6,31 +6,22 @@ void setup() {
 
   IMU.begin();    //allume IMU
 
-  Serial.println("Acceleration in g's");
-  Serial.println("X\tY\tZ");
 }
 
 void loop() {
   float x, y, z;
-  
-  Serial.print("Accelerometre");    //que on comprenne quelle chiffre est quoi
-  Serial.println();   //laisse une ligne vide
-  
   IMU.readAcceleration(x, y, z);    //precise x,y,z sont de l accelerometre
     Serial.print(x);
-    Serial.print('\t');   //laisse de l espace dans l affichage
+    Serial.print('\t');
     Serial.print(y);
     Serial.print('\t');
     Serial.println(z);
-
-  Serial.print("Gyroscope");
-  Serial.println();
-  
   IMU.readGyroscope(x, y, z);
     Serial.print(x);
     Serial.print('\t');
     Serial.print(y);
     Serial.print('\t');
     Serial.println(z);
-  delay(300);   //attend 3 secondes (en mili sec.)
+    Serial.println();
+  delay(3000);   //attend 3 secondes (en mili sec.)
 }
